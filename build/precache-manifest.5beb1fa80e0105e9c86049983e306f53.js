169eb4549ab3e9b69d24b5be3adf4767610aa7bb
Dairo Cantillo <cantillodairo62@gmail.com>
2020-10-05 19:13:15 -0500

    build

---
+++ build/precache-manifest.5beb1fa80e0105e9c86049983e306f53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5216eccde4c8bb84ebca0d8710792719",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46e5d59681098ac0ca72",
+    "url": "/static/css/main.4c798faf.chunk.css"
+  },
+  {
+    "revision": "eca6ecf59faab245e8e7",
+    "url": "/static/js/2.7b1b6907.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7b1b6907.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46e5d59681098ac0ca72",
+    "url": "/static/js/main.36b1752d.chunk.js"
+  },
+  {
+    "revision": "79ed7a821283744f3318",
+    "url": "/static/js/runtime-main.f752c8b4.js"
+  }
+]);
